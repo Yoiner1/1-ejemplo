bcf8473f5249b7c6f7923013f9182127dfe5a375
Ailan Ñañez <55756195+41L4N@users.noreply.github.com>
2024-11-06 11:52:48 -0400

    Estructura de menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
-
-import Navegacion from './navegacion';
+import Menu from './menu';
 
 function App() {
-  return ( 
-    <BrowserRouter>
-    <nav>
-      <Navegacion />
-      <Link to="/home">Inicio</Link>
-      <Link to="/about">Acerca de</Link>
-      <Link to="/contact">Contacto</Link>
-    </nav>
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
-
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Menu />
+			{/* <Link to="/home">Inicio</Link>
+			<Link to="/about">Acerca de</Link>
+			<Link to="/contact">Contacto</Link> */}
+			<Routes>
+				<Route path="/home" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-
-export default App 
+export default App;
--- src/menu.jsx
+++ src/menu.jsx
@@ -3,8 +3,6 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
@@ -12,48 +10,65 @@ import InboxIcon from '@mui/icons-material/MoveToInbox';
 import MailIcon from '@mui/icons-material/Mail';
 
 export default function TemporaryDrawer() {
-  const [open, setOpen] = React.useState(false);
 
-  const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
+	const [open, setOpen] = React.useState(false);
 
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
+	const routes = [
+		{
+			to: '/home',
+			label: 'Home',
+		},
+		{
+			to: '/about',
+			label: 'About',
+		},
+		{
+			to: '/contact',
+			label: 'Contact',
+		},
+	];
 
-  return (
-    <div>
-      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-    </div>
-  );
+	const toggleDrawer = (newOpen) => () => {
+		setOpen(newOpen);
+	};
+
+	const DrawerList = (
+		<Box
+			sx={{ width: 250 }}
+			role="presentation"
+			onClick={toggleDrawer(false)}
+		>
+			<List>
+				{
+					routes.map((route, index) => (
+						<ListItemButton
+							key={index}
+							to={route.to}
+						>
+							<ListItemIcon>
+								{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+							</ListItemIcon>
+							<ListItemText primary={route.label} />
+						</ListItemButton>
+					))
+				}
+			</List>
+		</Box>
+	);
+
+	return (
+		<div>
+			<Button
+				onClick={toggleDrawer(true)}
+			>
+				Open drawer
+			</Button>
+			<Drawer
+				open={open}
+				onClose={toggleDrawer(false)}
+			>
+				{DrawerList}
+			</Drawer>
+		</div>
+	);
 }
